eb4d15f3b5690b667077a29379cebb3795d805ca
Brian_064 <jarelbrian064@gmail.com>
2019-06-22 16:26:34 -0600

    update

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const bodyParser = require('body-parser')
+const PORT = process.env.PORT || 6464
+
+var urlEncodedParser = bodyParser.urlencoded({ extended:false })
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  // .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('pages/home'))
+  .post('/result', urlEncodedParser, function (req, res){
+    res.render('pages/results', {param1 : parseInt(req.body.param1), param2 : parseInt(req.body.param2)});
+  })
+  // .listen(PORT, () => console.log(`Listening on ${ PORT }`))
